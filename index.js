a18be540f817b7eaa6d2b51f38ab0afec8336e17
srushtikulkarni <srushtikulkarni09@gmail.com>
2025-12-17 14:56:51 +0530

    fixed: fetching task by id api

--- index.js
+++ index.js
@@ -614,7 +614,7 @@ app.get("/teams/:teamId/owners", async (req, res) => {
   }
 });
 
-// fetch task 
+// fetch task
 app.get("/tasks/search", async (req, res) => {
   try {
     const { name } = req.query;
@@ -645,19 +645,26 @@ app.get("/tasks/search", async (req, res) => {
 });
 
 
-// fetching project details
+// fetching task details by id
 
+app.get("/tasks/search/:id",async(req,res)=>{
+  try{
+    const id = req.params.id;
 
-// app.get("/projectDetails/:id",async(req,res)=>{
-//   try{
-//     const id = req.params.id;
+    const response = await Task.findOne({_id:id});
+    console.log(response);
 
-//     const findProjectData = await Project.findOne({id:id}).populate()
+    if(!response){
+      return res.status(404).json({error:"Task is not found"})
+    }
 
-//   }catch(error){
-//     res.status(500).json({message:"Internal Server Error",error: error.message});
-//   }
-// })
+   res.status(200).json({message:"Successfully fetched the task data",response});
+
+  }catch(error){
+    res.status(200).json({message:"Internal Server Error",error:error.message});
+  }
+})
+
 
 
 
